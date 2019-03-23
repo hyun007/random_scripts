d77c718addf4cd6393c97037e9383aad3fa16307
Hyun Jo <hyunjo@gmail.com>
2019-03-23 11:32:31 -0400

    Update dotfile_setup.sh

--- dotfile_setup.sh
+++ dotfile_setup.sh
@@ -1,7 +1,7 @@
 #source: https://developer.atlassian.com/blog/2016/02/best-way-to-store-dotfiles-git-bare-repo/
 #call me with:
 #curl -Lks https://raw.githubusercontent.com/hyun007/random_scripts/master/dotfile_setup.sh | /bin/bash
-git clone --bare git@github.com:hyun007/dotfiles.git $HOME/.cfg
+git clone --bare https://github.com/hyun007/dotfiles.git $HOME/.cfg
 function config {
    /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
 }
