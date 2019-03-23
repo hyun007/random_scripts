ca2f35a0619e0860cb8e5119ee372db1a74dc8ee
Hyun Jo <hyunjo@gmail.com>
2019-03-23 02:14:28 -0400

    Update dotfile_setup.sh

--- dotfile_setup.sh
+++ dotfile_setup.sh
@@ -1,3 +1,5 @@
+#call me with
+#curl -Lks https://raw.githubusercontent.com/hyun007/random_scripts/master/dotfile_setup.sh | /bin/bash
 git clone --bare git@github.com:hyun007/dotfiles.git $HOME/.cfg
 function config {
    /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
