ac44c993e9441a10b42942123e0b968d5add92c5
Hyun Jo <hyunjo@gmail.com>
2019-03-23 01:16:45 -0400

    Update shell_setup.sh

--- shell_setup.sh
+++ shell_setup.sh
@@ -1,2 +1,4 @@
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_awesome_vimrc.sh
