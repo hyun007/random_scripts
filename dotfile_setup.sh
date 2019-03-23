bb8689bcbfdebe2c603169a6532eabdc843508a5
Hyun Jo <hyunjo@gmail.com>
2019-03-23 15:06:37 -0400

    backs up old stuff

--- dotfile_setup.sh
+++ dotfile_setup.sh
@@ -1,12 +1,20 @@
 #source: https://developer.atlassian.com/blog/2016/02/best-way-to-store-dotfiles-git-bare-repo/
 #call me with:
 #curl -Lks https://raw.githubusercontent.com/hyun007/random_scripts/master/dotfile_setup.sh | /bin/bash
+cd ~
+
+if [ -d ".config-backup" ]; then
+  echo ".config-backup exists"
+  mv .config-backup ".config-backup-$(date +"%s")"
+fi
+
+mkdir -p .config-backup/.ssh
+mv .ssh/config .config-backup/.ssh
+mv .cfg .config-backup/
 git clone --bare https://github.com/hyun007/dotfiles.git $HOME/.cfg
 function config {
    /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
 }
-mkdir -p .config-backup/.ssh
-mv .ssh/config .config-backup/.ssh
 config checkout
 if [ $? = 0 ]; then
   echo "Checked out config.";
